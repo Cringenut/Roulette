a084c56fd372df798bf58fb06d5d40440d167188
Cringenut <ihnatbaikou@gmail.com>
2023-05-13 21:22:50 +0200

    Roulette ver 0.1.0

---
+++ Pages/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>GoldenStar</title>
+        <link href="./../Visual/index/index.css" rel="stylesheet" type="text/css">
+    </head>
+
+    <body>
+        <div class="box-top">
+            <div class="box-down">
+    </body>
+</html>
+
+
+<?php
---
+++ Pages/roulette.php
@@ -0,0 +1,23 @@
+div{
+bac
+}
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Roulette</title>
+
+</head>
+<body>
+<h1>Page1</h1>
+<a href="index.php">Index</a>
+</body>
+
+</html>
+
+
+
+
+<?php
