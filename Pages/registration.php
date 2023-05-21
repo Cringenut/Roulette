85e1eba88b5fb8e9578cef704ed61627d31a2b90
Cringenut <ihnatbaikou@gmail.com>
2023-05-21 20:23:04 +0200

    Roulette ver 0.2.3

---
+++ Pages/registration.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Golden Star: login</title>
+    <link href="./../Visual/login/registration.css" rel="stylesheet" type="text/css">
+
+
+    <div class="box-logo-holder">
+        <img class="img-logo-big" src="../Visual/index/png/logo.png"  alt="logo.png"/>
+    </div>
+
+    <div class="box-login-holder">
+        <div class="text-login-field"</div>
+    <input type="login">
+    <div class="text-password-field"</div>
+    <input type="password">
+    </div>
+
+</head>
+
+<?php
