d9fe4dc1398050fe2ff922a972a9c337c94c5d91
Cringenut <ihnatbaikou@gmail.com>
2023-06-11 11:37:27 +0200

    Roulette ver 0.4.1

--- Pages/login.php
+++ Pages/login.php
@@ -3,38 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <title>Golden Star: login</title>
-    <link href="./../Visual/login/login.css" rel="stylesheet" type="text/css">
+    <link href="../Visual/login/login.css" rel="stylesheet" type="text/css">
 
     <div class="img-background">
         <img class="img-background" src="../Visual/login/png/login.png"  alt="logo.png"/>
     </div>
 
-    <div class="box-logo-holder">
-        <img class="img-logo-big" src="../Visual/index/png/logo.png"  alt="logo.png"/>
-    </div>
+    <a class="" href="index.php" target="_self">
+        <div class="box-logo-holder">
+            <img class="img-logo-big" src="../Visual/index/png/logo.png"  alt="logo.png"/>
+        </div>
+    </a>
 
     <div class="box-login-holder">
         <div class="break"></div>
-        <div class="box-login-register-holder">
-            <div class="btn-sign-up-select"></div>
+        <div class="box-register-login-holder">
+            <a class="btn-sign-up-select" href="registration.php" target="_self">
+                <div class="btn-sign-up-select">
+                    <div class="text-sign-up">SIGN UP</div>
+                </div>
+            </a>
             <div class="break-login-register"></div>
-            <div class="btn-sign-in-select"></div>
+            <div class="btn-sign-in-select">
+                <div class="text-sign-in">SIGN IN</div>
+            </div>
         </div>
 
-        <div class="box-login-text">
+        <div class="box-registration-text">
             <div class="text-login"</div>LOGIN</div>
-        </div>
-        <div class="box-text-login"></div>
-            <input type="text" placeholder="Login">
-        <div class="break"></div>
-        <div class="box-text-login1"></div>
-            <input type="password" placeholder="Password">
-        <div class="break"></div>
-        <div class="break"></div>
-        <div class="btn-sign-up"></div>
+    </div>
+    <input type="text" placeholder="Login">
+    <div class="break"></div>
+    <input type="password" placeholder="Password">
+    <div class="break"></div>
+    <div class="break"></div>
+    <div class="btn-sign-in"></div>
     </div>
 
-
 </head>
+
 
 <?php
--- Pages/registration.php
+++ Pages/registration.php
@@ -9,34 +9,38 @@
         <img class="img-background" src="../Visual/registration/png/registration.png"  alt="logo.png"/>
     </div>
 
-    <div class="box-logo-holder">
-        <img class="img-logo-big" src="../Visual/index/png/logo.png"  alt="logo.png"/>
-    </div>
+    <a class="" href="index.php" target="_self">
+        <div class="box-logo-holder">
+            <img class="img-logo-big" src="../Visual/index/png/logo.png"  alt="logo.png"/>
+        </div>
+    </a>
 
-    <div class="box-login-holder">
+    <div class="box-registration-holder">
         <div class="break"></div>
-        <div class="box-login-register-holder">
+        <div class="box-register-login-holder">
             <div class="btn-sign-up-select">
                 <div class="text-sign-up">SIGN UP</div>
             </div>
             <div class="break-login-register"></div>
-            <div class="btn-sign-in-select">
-                <div class="text-sign-in">SIGN IN</div>
-            </div>
+            <a class="btn-sign-in-select" href="login.php" target="_self">
+                <div class="btn-sign-in-select">
+                    <div class="text-sign-in">SIGN IN</div>
+                </div>
+            </a>
         </div>
 
-        <div class="box-login-text">
-            <div class="text-login"</div>REGISTRATION</div>
+        <div class="box-registration-text">
+            <div class="text-registration"</div>REGISTRATION</div>
     </div>
-    <div class="box-text-login"></div>
     <input type="text" placeholder="E-mail">
     <div class="break"></div>
-    <div class="box-text-login"></div>
     <input type="text" placeholder="Login">
     <div class="break"></div>
-    <div class="box-text-login1"></div>
     <input type="password" placeholder="Password">
     <div class="break"></div>
+    <div class="wrapper">
+        <input type="checkbox" id="check">
+    </div>
     <div class="break"></div>
     <div class="btn-sign-up"></div>
     </div>
