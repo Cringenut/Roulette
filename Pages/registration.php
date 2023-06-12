f9fa95d0b9cae9dc60da6953775dec4491aa2ced
Cringenut <ihnatbaikou@gmail.com>
2023-06-12 23:29:33 +0200

    Roulette ver 0.4.6

--- Pages/registration.php
+++ Pages/registration.php
@@ -16,55 +16,55 @@ function checkMailExistence($address)
 <head>
     <meta charset="UTF-8">
     <title>Golden Star: registration</title>
-    <body onload="check();">
-    </body>
+<body onload="check();">
+</body>
 
-    <link href="../Visual/registration/registration.css" rel="stylesheet" type="text/css">
+<link href="../Visual/registration/registration.css" rel="stylesheet" type="text/css">
 
-    <div class="img-background">
-        <img class="img-background" src="../Visual/registration/png/registration. png"  alt="logo.png"/>
+<div class="img-background">
+    <img class="img-background" src="../Visual/registration/png/registration. png"  alt="logo.png"/>
+</div>
+
+<a class="" href="index.php" target="_self">
+    <div class="box-logo-holder">
+        <img class="img-logo-big" src="../Visual/index/png/logo.png"  alt="logo.png"/>
     </div>
+</a>
 
-    <a class="" href="index.php" target="_self">
-        <div class="box-logo-holder">
-            <img class="img-logo-big" src="../Visual/index/png/logo.png"  alt="logo.png"/>
+<form action = "" method = "post">
+
+<div class="box-registration-holder">
+    <div class="break"></div>
+    <div class="box-register-login-holder">
+        <div class="btn-sign-up-select">
+            <div class="text-sign-up">SIGN UP</div>
         </div>
-    </a>
-
-    <div class="box-registration-holder">
-        <div class="break"></div>
-        <div class="box-register-login-holder">
-            <div class="btn-sign-up-select">
-                <div class="text-sign-up">SIGN UP</div>
+        <div class="break-login-register"></div>
+        <a class="btn-sign-in-select" href="login.php" target="_self">
+            <div class="btn-sign-in-select">
+                <div class="text-sign-in">SIGN IN</div>
             </div>
-            <div class="break-login-register"></div>
-            <a class="btn-sign-in-select" href="login.php" target="_self">
-                <div class="btn-sign-in-select">
-                    <div class="text-sign-in">SIGN IN</div>
-                </div>
-            </a>
-        </div>
-
-        <div class="box-registration-text">
-            <div class="text-registration"</div>REGISTRATION</div>
+        </a>
     </div>
 
-    <form action = "" method = "post">
-        <input type="text" name="email" id="email   " onkeyup="check(this.value);" placeholder="E-mail">
+    <div class="box-registration-text">
+        <div class="text-registration"</div>REGISTRATION</div>
+</div>
+
+
+    <input type="text" name="email" id="email   " onkeyup="check(this.value);" placeholder="E-mail">
     <div class="break"></div>
-        <input type="text" name="login" id="login" onkeyup="check(this.value);" placeholder="Login">
+    <input type="text" name="login" id="login" onkeyup="check(this.value);" placeholder="Login">
     <div class="break"></div>
-        <input type="text" name="password" id="password" onkeyup="check(this.value);" placeholder="Password">
+    <input type="text" name="password" id="password" onkeyup="check(this.value);" placeholder="Password">
 
     <div class="break"></div>
     <div class="break"></div>
 
 
-        <button name="signup" id="signup" type="submit" class="btn-sign-up"></button>
-    </form>
+    <button name="signup" id="signup" type="submit" class="btn-sign-up"></button>
 
-
-
+</form>
 
     <body>
 
